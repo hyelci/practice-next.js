38da92b1b680622be82cffe1e7db87af4665d5bf
Hacer Yelci <haceryelci@gmail.com>
2023-09-01 11:00:32 +0100

    removed tailwind config

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,6 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
-// import Date from "../../components/date";
 import Date from "../../components/Date";
 
 export default function Post({ postData }) {
